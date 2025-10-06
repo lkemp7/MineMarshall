34fa68b97270259e3f1a657f144db04c07d11cf3
Josh <jdkeen@live.com>
2025-10-06 19:18:50 +1100

    Sidbar Addition to the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,3 +7,8 @@ def dashboard(request):
     users = CustomUser.objects.all()
     context = {'users': users}
     return render(request, 'dashboard.html', context)
+
+@login_required
+def personnel(request):
+    users = CustomUser.objects.all().order_by("first_name", "last_name")
+    return render(request, 'personnel.html', {"users": users})
